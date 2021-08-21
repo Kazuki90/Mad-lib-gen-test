98d40a7d32de4d8479c8eefed99d50b38bedf5d1
Kazuki90 <86367831+Kazuki90@users.noreply.github.com>
2021-08-20 21:13:05 -0400

    Add files via upload

---
+++ MadLib.py
@@ -0,0 +1,22 @@
+#MadLib gen test 
+print("Welcome to MadLib gen test")
+
+x = #type in  adjective with ""
+y = #type in adjective with "" 
+z = #type in verb with ""    
+b = #type in noun with ""  
+
+#Madlib gen test one
+
+madlib = "The big cat {} played {} with the little mouse and they went to {} after the {} ended" #madlib 1
+print(madlib.format(x, y, z, b))
+
+print("This is not a very good madib, it is only for testing purposes right now")
+
+
+
+
+
+
+
+
